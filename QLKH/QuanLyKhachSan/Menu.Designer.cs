d1cd9e62964c7c994eafc0700e4fc51e988c48a9
duong <duongnvt2110@gmail.com>
2018-05-20 15:29:56 +0700

    fix design

--- QLKH/QuanLyKhachSan/Menu.Designer.cs
+++ QLKH/QuanLyKhachSan/Menu.Designer.cs
@@ -69,6 +69,7 @@
             this.btnBaoCao.Text = "Báo cáo";
             this.btnBaoCao.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btnBaoCao.UseVisualStyleBackColor = false;
+            this.btnBaoCao.Click += new System.EventHandler(this.btnBaoCao_Click);
             // 
             // btnThoat
             // 
