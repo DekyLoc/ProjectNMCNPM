dcb98ee2c2d9e8e5703f15b8b45e45755a72e2d6
huyhaonguyen <32436989+huyhaonguyen@users.noreply.github.com>
2018-05-21 21:24:06 +0700

    HD-Thanh-toan-design

--- QLKH/QuanLyKhachSan/HoaDonThanhToan.Designer.cs
+++ QLKH/QuanLyKhachSan/HoaDonThanhToan.Designer.cs
@@ -384,7 +384,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(736, 609);
+            this.ClientSize = new System.Drawing.Size(736, 552);
             this.Controls.Add(this.txtDuaLaiKhach);
             this.Controls.Add(this.txtNhanTuKhach);
             this.Controls.Add(this.label11);
